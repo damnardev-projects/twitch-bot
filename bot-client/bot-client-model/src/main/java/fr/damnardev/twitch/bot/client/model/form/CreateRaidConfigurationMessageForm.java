562f3ac095c037a0e16d95bbcebf332d8936909a
damnardev <184518315+damnardev@users.noreply.github.com>
2025-01-09 20:03:03 +0100

    fix: dead code & jar name (#179)

--- bot-client/bot-client-model/src/main/java/fr/damnardev/twitch/bot/client/model/form/CreateRaidConfigurationMessageForm.java
+++
@@ -1,8 +0,0 @@
-package fr.damnardev.twitch.bot.client.model.form;
-
-import lombok.Builder;
-
-@Builder
-public record CreateRaidConfigurationMessageForm(Long channelId, String channelName,
-		String message) {
-}
